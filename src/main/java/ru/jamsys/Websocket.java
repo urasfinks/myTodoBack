6ea5d3945ca184c1ab7ca306040e42b90e8dac37
uramuhin <urasfinks@yandex.ru>
2022-06-21 01:53:35 +0300

    DB Update data state

--- src/main/java/ru/jamsys/Websocket.java
+++ src/main/java/ru/jamsys/Websocket.java
@@ -1,6 +1,9 @@
 package ru.jamsys;
 
 import com.google.gson.Gson;
+import ru.jamsys.database.Database;
+import ru.jamsys.database.DatabaseArgumentDirection;
+import ru.jamsys.database.DatabaseArgumentType;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
@@ -14,10 +17,82 @@ import java.util.concurrent.atomic.AtomicLong;
 @ServerEndpoint("/websocket")
 public class Websocket {
 
+    class State {
+
+        final String dataUID;
+        final private Map<String, Object> state = new ConcurrentHashMap<>();
+
+        public long getIndexRevision() {
+            return indexRevision.get();
+        }
+
+        final private AtomicLong indexRevision = new AtomicLong(0);
+
+        State(String dataUID) {
+            this.dataUID = dataUID;
+            loadFromDb();
+        }
+
+        public void update(String key, Object value) {
+            boolean upd = false;
+            if (!state.containsKey(key) || !state.get(key).equals(value)) {
+                upd = true;
+                state.put(key, value);
+            }
+            if(upd){
+                indexRevision.incrementAndGet();
+                loadToDb();
+            }
+        }
+
+        private void loadToDb() {
+            try {
+                Database database = new Database();
+                database.addArgument("uid_data", DatabaseArgumentType.VARCHAR, DatabaseArgumentDirection.IN, dataUID);
+                database.addArgument("state_data", DatabaseArgumentType.VARCHAR, DatabaseArgumentDirection.IN, new Gson().toJson(state));
+                database.addArgument("revision_state_data", DatabaseArgumentType.NUMBER, DatabaseArgumentDirection.IN, indexRevision.get());
+                database.exec("java:/PostgreDS", "update data set state_data = ${state_data}, revision_state_data = ${revision_state_data} where uid_data = ${uid_data}");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void loadFromDb() {
+            try {
+                Database database = new Database();
+                database.addArgument("uid_data", DatabaseArgumentType.VARCHAR, DatabaseArgumentDirection.IN, dataUID);
+                database.addArgument("revision_state_data", DatabaseArgumentType.NUMBER, DatabaseArgumentDirection.COLUMN, null);
+                database.addArgument("state_data", DatabaseArgumentType.VARCHAR, DatabaseArgumentDirection.COLUMN, null);
+                List<Map<String, Object>> exec = database.exec("java:/PostgreDSR", "select state_data, revision_state_data from data where uid_data = ${uid_data}");
+                System.out.println(exec);
+                if (exec.size() > 0) {
+                    Object revisionStateData = exec.get(0).get("revision_state_data");
+                    if(revisionStateData != null && !"".equals(revisionStateData.toString())){
+                        indexRevision.set(Long.parseLong(revisionStateData.toString()));
+                    }
+                    Object stateData = exec.get(0).get("state_data");
+                    if(stateData != null && !"".equals(stateData.toString())){
+                        Map<String, Object> dbState = new Gson().fromJson(stateData.toString(), Map.class);
+                        for (String key : dbState.keySet()) {
+                            state.put(key, dbState.get(key));
+                        }
+                    }
+                    System.out.println("Load from DB for DataUID = '" + dataUID + "' indexRevision = " + indexRevision.get() + "; state = " + state.toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     class DataRevision {
 
         private List<Session> sessions = new ArrayList<>();
-        private AtomicLong indexRevision = new AtomicLong(0);
+        final State state;
+
+        public DataRevision(String dataUID) {
+            this.state = new State(dataUID);
+        }
 
         public void addSession(Session session) {
             sessions.add(session);
@@ -28,11 +103,13 @@ public class Websocket {
         }
 
         public void notify(Session session, String dataUID, Map<String, Object> data) {
+            Map<String, Object> jsonData = (Map<String, Object>) data.get("Data");
+            state.update(jsonData.get("key").toString(), jsonData.get("value"));
 
             Map<String, Object> x = new HashMap<>();
             x.put("DataUID", dataUID);
-            x.put("Action", "update_revision");
-            x.put("Revision", indexRevision.incrementAndGet());
+            x.put("Action", Action.UPDATE_REVISION.toString());
+            x.put("Revision", state.getIndexRevision());
 
             try {
                 session.getBasicRemote().sendText(new Gson().toJson(x));
@@ -40,7 +117,7 @@ public class Websocket {
                 e.printStackTrace();
             }
 
-            data.put("Revision", indexRevision.get());
+            data.put("Revision", state.getIndexRevision());
             String dataSend = new Gson().toJson(data);
 
             for (Session ses : sessions) {
@@ -58,7 +135,7 @@ public class Websocket {
         public String toString() {
             return "DataRevision{" +
                     "sessions=" + sessions +
-                    ", indexRevision=" + indexRevision +
+                    ", indexRevision=" + state.getIndexRevision() +
                     '}';
         }
 
@@ -77,10 +154,10 @@ public class Websocket {
         if (jsonParsed.containsKey("DataUID") && jsonParsed.containsKey("Action")) {
             String dataUID = (String) jsonParsed.get("DataUID");
             String actionType = (String) jsonParsed.get("Action");
-            if(dataUID == null){
+            if (dataUID == null) {
                 return null;
             }
-            if(actionType == null){
+            if (actionType == null) {
                 return null;
             }
             try {
@@ -88,7 +165,7 @@ public class Websocket {
                 switch (action) {
                     case SUBSCRIBE:
                         if (!mapDataUID.containsKey(dataUID)) {
-                            mapDataUID.put(dataUID, new DataRevision());
+                            mapDataUID.put(dataUID, new DataRevision(dataUID));
                         }
                         mapDataUID.get(dataUID).addSession(session);
                         if (!mapSession.containsKey(session)) {
@@ -106,7 +183,7 @@ public class Websocket {
                         unsubscribe(session, dataUID);
                         break;
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -130,10 +207,11 @@ public class Websocket {
         }
     }
 
-    private void unsubscribe(Session session, String dataUID){
+    private void unsubscribe(Session session, String dataUID) {
         mapDataUID.get(dataUID).removeSession(session);
         if (mapDataUID.get(dataUID).getSessionSize() == 0) {
             mapDataUID.remove(dataUID);
         }
     }
+
 }
