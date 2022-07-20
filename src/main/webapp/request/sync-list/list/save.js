5096aff0a67b195791fb1781a75a11f6b4f81641
urasfinks <urasfinks@yandex.ru>
2022-07-20 17:21:59 +0300

    List process

--- src/main/webapp/request/sync-list/list/save.js
+++ src/main/webapp/request/sync-list/list/save.js
@@ -1,12 +1,17 @@
 function main(state, rc, content) {
     //content.addData({title: "RC:" + rc.toString()}, "Text");
-    if(rc.getParam.uid_data != undefined){
+    if (rc.getParam.uid_data != undefined) {
         var newDataUid = Java.type('ru.jamsys.JS').addData(rc, state, [rc.getParam.uid_data]);
         var data = {};
-        data['time_'+newDataUid] = new Date().getTime();
+        data[newDataUid] = false;
         Java.type('ru.jamsys.JS').updateDataState(rc, rc.getParam.uid_data, JSON.stringify(data));
     }
     content.addData({title: "Opacha"}, "DialogOk");
     content.addAction("closeWindow", {data: {delay: 1000, count: 2}});
-    content.addAction("reloadPageByUrl", {"list": ["/project/" + rc.projectName + "/list"]});
+    content.addAction("reloadPageByUrl", {
+        "list": [
+            "/project/" + rc.projectName + "/list",
+            "/project/" + rc.projectName
+        ]
+    });
 }
--- src/main/webapp/request/sync-list/main.js
+++ src/main/webapp/request/sync-list/main.js
@@ -6,6 +6,57 @@ function main(state, rc, content) {
         onPressedData: {url: rc.url + "/add", title: "Добавить список"},
         icon: "playlist_add"
     }, "AppBarActionAdd");
+    var list = getList(rc);
+    if (list.length > 0) {
+        content.addData({}, "GroupTop");
+        for (var i = 0; i < list.length; i++) {
+            var data = JSON.parse(list[i]["state_data"]);
+            if (i != 0) {
+                content.addData({}, "Divider");
+            }
+            var countActive = 0;
+            var countComplete = 0;
+            for (var k in data) {
+                if (k.startsWith("time_")) {
+                    if (data[k.split("time_")[1]] == true) {
+                        countActive++;
+                    } else {
+                        countComplete++;
+                    }
+                }
+            }
+            //content.addData({title: list[i]["state_data"]}, "Text");
+            content.addData({
+                title: data["name"],
+                badge1: countComplete + "",
+                badge2: countActive + "",
+                onTapData: {
+                    title: data["name"],
+                    dataUID: list[i]["uid_data"],
+                    url: rc.url + "/list?uid_data=" + list[i]["uid_data"],
+                    backgroundColor: "#f5f5f5"
+                }
+            }, "RowInkWellBadge2");
+        }
+        content.addData({}, "GroupBottom");
+    } else {
+        content.addData({title: "Создай новый список задач, нажав на кнопку в правом верхнем углу"}, "EmptyList");
+        content.addData({height: 20, width: 10}, "SizedBox");
+        content.addData({title: "Что это такое?"}, "H1");
+        content.addData({
+            marker: "1",
+            title: "Можно создать несколько списков задач, например \"Купить в магазине\" или \"Взять с собой в отпуск\""
+        }, "TextDescription");
+        content.addData({
+            marker: "2",
+            title: "Возможность вести совместные списки с близкими людьми, например общая организация сбора в поход"
+        }, "TextDescription");
+        content.addData({marker: "3", title: "Контролировать исполнение не погружаясь в детали"}, "TextDescription");
+    }
+}
+
+
+function getList(rc) {
     var list = [];
     try {
         var obj = {
@@ -38,30 +89,5 @@ function main(state, rc, content) {
         var list = JSON.parse(Java.type('ru.jamsys.JS').sql(JSON.stringify(obj)));
     } catch (e) {
     }
-    if(list.length > 0){
-        content.addData({}, "GroupTop");
-        for (var i = 0; i < list.length; i++) {
-            var data = JSON.parse(list[i]["state_data"]);
-            if(i != 0){
-                content.addData({}, "Divider");
-            }
-            content.addData({
-                title: data["name"],
-                onTapData: {
-                    title: data["name"],
-                    dataUID: list[i]["uid_data"],
-                    url: rc.url + "/list?uid_data=" + list[i]["uid_data"],
-                    backgroundColor: "#f5f5f5"
-                }
-            }, "RowInkWell");
-        }
-        content.addData({}, "GroupBottom");
-    } else {
-        content.addData({title: "Создай новый список задач, нажав на кнопку в правом верхнем углу"}, "EmptyList");
-        content.addData({height: 20, width: 10}, "SizedBox");
-        content.addData({title: "Что это такое?"}, "H1");
-        content.addData({marker: "1", title: "Можно создать несколько списков задач, например \"Купить в магазине\" или \"Взять с собой в отпуск\""}, "TextDescription");
-        content.addData({marker: "2", title: "Возможность вести совместные списки с близкими людьми, например общая организация сбора в поход"}, "TextDescription");
-        content.addData({marker: "3", title: "Контролировать исполнение не погружаясь в детали"}, "TextDescription");
-    }
+    return list;
 }
