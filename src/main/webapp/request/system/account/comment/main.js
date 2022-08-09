a7993f1e3c45d73b2a34e740c4fe11e271e53daf
uramuhin <urasfinks@yandex.ru>
2022-08-09 16:26:57 +0300

    ReUI /account/comment

--- src/main/webapp/request/system/account/comment/main.js
+++ src/main/webapp/request/system/account/comment/main.js
@@ -1,15 +1,17 @@
 function main(state, rc, content) {
     //content.addData({title: "RC:" + rc.toString()}, "Text");
+    content.setWidgetData("backgroundColor", "#f5f5f5");
+    content.setSeparated(false);
     content.setParentUI("WrapPage20");
-    content.addData({title: "Напиши что-либо нам"}, "FirstH1-P-0-20");
+    content.addData({title: "Напиши нам что-нибудь"}, "FirstH1-P-0-20");
     content.addData({
         type: "multiline",
-        label: "Комментарий / предложение",
+        hint: "Комментарий / предложение",
         data: "",
         name: "comment",
         minLines: 3
     }, "TextEdit");
-    content.addData({height: 30, width: 10}, "SizedBox");
+    content.addData({height: 20, width: 10}, "SizedBox");
     content.addData({
         title: "Отправить",
         icon: "save",
