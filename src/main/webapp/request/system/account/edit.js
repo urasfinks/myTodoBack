dd67cef558c3e085825153d71e86701ab7407203
uramuhin <urasfinks@yandex.ru>
2022-08-09 15:24:00 +0300

    ReUI /account/edit

--- src/main/webapp/request/system/account/edit.js
+++ src/main/webapp/request/system/account/edit.js
@@ -1,15 +1,18 @@
 function main(state, rc, content) {
     content.setParentUI("WrapPage20");
 
+    content.setWidgetData("backgroundColor", "#f5f5f5");
+
     var res = JSON.parse(Java.type('ru.jamsys.JS').getPersonState(rc, JSON.stringify({
         "fio": "",
         "bday": "",
         "gender": 0
     })));
 
-    content.addData({title: "Заполни информацию о себе"}, "H1-P-0-20");
+    content.addData({title: "Заполни информацию о себе"}, "FirstH1-P-0-20");
 
     content.addData({type: "text", label: "Имя Отчество", data: res["fio"], name: "fio"}, "TextEdit");
+    content.addData({height: 10, width: 0}, "SizedBox");
     content.addData({type: "datetime", label: "Дата рождения", data: res["bday"], name: "bday"}, "TextEdit");
     content.addData({height: 20, width: 0}, "SizedBox");
 
