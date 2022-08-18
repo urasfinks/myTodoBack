dcb809e74531cf793251d4cb9a1f2988e15796be
uramuhin <urasfinks@yandex.ru>
2022-08-18 21:51:01 +0300

    BridgeState process

--- src/main/webapp/request/sync-list/list/add/notify.js
+++ src/main/webapp/request/sync-list/list/add/notify.js
@@ -8,13 +8,14 @@ function main(state, rc, content) {
         title: "Тип оповещения",
         name: "notify",
         selectedIndex: 0,
-        value: "standard",
+        value: "none",
         margin: "5,0,5,0"
     }, "DropdownRadio", {
         items: JSON.stringify([
+            {title: "Нет", value: "none"},
             {title: "Стандартное", value: "standard"},
             {title: "Единоразовое", value: "once"},
-            {title: "Циклическо", value: "cycle"},
+            {title: "Циклическое", value: "cycle"},
             {title: "Выборочное", value: "custom"}
         ])
     });
@@ -25,6 +26,7 @@ function main(state, rc, content) {
     standard(state, rc, content);
     once(state, rc, content);
     cycle(state, rc, content);
+    custom(state, rc, content);
 
     content.addData({height: 10, width: 10}, "SizedBox");
 
@@ -34,7 +36,7 @@ function main(state, rc, content) {
         desc: "",
         descColor: "grey",
         titleColor: "black",
-        color: "white",
+        color: "transparent",
         onTapData: {
             title: "План уведомлений",
             url: rc.url + "/notify/plan",
@@ -42,8 +44,15 @@ function main(state, rc, content) {
             bridgeState: {
                 notify: "standard"
             }
+        },
+        arg: "standard",
+        standard: {
+            "key": "notify",
+            "value": ["standard", "once", "cycle", "custom"],
+            "trueCondition": "true",
+            "falseCondition": "false"
         }
-    }, "RowInkWell");
+    }, "RowInkWell", "NotifyStandard");
     content.addData({}, "GroupBottom");
 }
 
@@ -88,6 +97,8 @@ function cycle(state, rc, content){
 
     hour(state, rc, content);
     day(state, rc, content);
+    week(state, rc, content);
+    month(state, rc, content);
 
     content.addData({
         title: "Количество повторений:",
@@ -126,6 +137,18 @@ function cycle(state, rc, content){
             "falseCondition": "false"
         }
     }, "TextEdit", "NotifyStandard");
+
+    content.addData({
+        title: "Начиная с выбранной даты, будут приходить оповещения в указанное время через выбранный интервал. Если не указывать количество повторений, то оповещения будут проиходить на протяжениии всего времени существования)",
+        padding: "10,17,10,10",
+        arg: "standard",
+        standard: {
+            "key": "notify",
+            "value": "cycle",
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "TextPadding", "NotifyStandard");
 }
 
 function once(state, rc, content){
@@ -156,13 +179,62 @@ function standard(state, rc, content){
     }, "TextPadding", "NotifyStandard");
 }
 
+function hour(state, rc, content){
+    content.addData({
+        title: "Оповещать каждые:",
+        padding: "12,15,0,15",
+        arg: "standard",
+        standard: {
+            "valueGroup": {
+                g1: {
+                    key: "notify",
+                    list: ["cycle"],
+                    condition: "and"
+                },
+                g2: {
+                    key: "interval",
+                    list: ["hour"],
+                    condition: "and"
+                }
+            },
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "TextPadding", "NotifyStandard");
+
+    content.addData({
+        type: "time",
+        hint: "Оповещать каждые",
+        label: "",
+        data: "01:00",
+        name: "interval_hour",
+
+        arg: "standard",
+        standard: {
+            "valueGroup": {
+                g1: {
+                    key: "notify",
+                    list: ["cycle"],
+                    condition: "and"
+                },
+                g2: {
+                    key: "interval",
+                    list: ["hour"],
+                    condition: "and"
+                }
+            },
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "TextEdit", "NotifyStandard");
+}
+
 function day(state, rc, content){
     content.addData({
         height: 10,
         width: 10,
         arg: "standard",
         standard: {
-            "key": "notify",
             "valueGroup": {
                 g1: {
                     key: "notify",
@@ -243,10 +315,10 @@ function day(state, rc, content){
     content.addData({}, "GroupBottom");
 }
 
-function hour(state, rc, content){
+function week(state, rc, content){
     content.addData({
-        title: "Оповещать каждые:",
-        padding: "12,15,0,15",
+        height: 10,
+        width: 10,
         arg: "standard",
         standard: {
             "valueGroup": {
@@ -257,21 +329,22 @@ function hour(state, rc, content){
                 },
                 g2: {
                     key: "interval",
-                    list: ["hour"],
+                    list: ["week"],
                     condition: "and"
                 }
             },
             "trueCondition": "true",
             "falseCondition": "false"
         }
-    }, "TextPadding", "NotifyStandard");
+    }, "SizedBox", "NotifyStandard");
 
+    content.addData({}, "GroupTop");
     content.addData({
-        type: "time",
-        hint: "Оповещать каждые",
-        label: "",
-        data: "01:00",
-        name: "deadLineTime_hour",
+        title: "Каждую/ые",
+        name: "interval_week",
+        selectedIndex: 0,
+        value: "1day",
+        margin: "5,0,5,0",
 
         arg: "standard",
         standard: {
@@ -283,24 +356,99 @@ function hour(state, rc, content){
                 },
                 g2: {
                     key: "interval",
-                    list: ["hour"],
+                    list: ["week"],
                     condition: "and"
                 }
             },
             "trueCondition": "true",
             "falseCondition": "false"
         }
-    }, "TextEdit", "NotifyStandard");
+    }, "DropdownRadio", {
+        items: JSON.stringify([
+            {title: "Неделю", value: "1day"},
+            {title: "Полторы недели", value: "1_5week"},
+            {title: "2 недели", value: "2week"},
+            {title: "3 недели", value: "3week"}
+        ])
+    }, "NotifyStandard");
+    content.addData({}, "GroupBottom");
+}
+
+function month(state, rc, content){
+    content.addData({
+        height: 10,
+        width: 10,
+        arg: "standard",
+        standard: {
+            "valueGroup": {
+                g1: {
+                    key: "notify",
+                    list: ["cycle"],
+                    condition: "and"
+                },
+                g2: {
+                    key: "interval",
+                    list: ["month"],
+                    condition: "and"
+                }
+            },
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "SizedBox", "NotifyStandard");
+
+    content.addData({}, "GroupTop");
+    content.addData({
+        title: "Каждый/ые",
+        name: "interval_month",
+        selectedIndex: 0,
+        value: "1month",
+        margin: "5,0,5,0",
+
+        arg: "standard",
+        standard: {
+            "valueGroup": {
+                g1: {
+                    key: "notify",
+                    list: ["cycle"],
+                    condition: "and"
+                },
+                g2: {
+                    key: "interval",
+                    list: ["month"],
+                    condition: "and"
+                }
+            },
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "DropdownRadio", {
+        items: JSON.stringify([
+            {title: "Месяц", value: "1month"},
+            {title: "Полтора месяца", value: "1_5month"},
+            {title: "2 месяца", value: "2month"},
+            {title: "3 месяца", value: "3month"},
+            {title: "4 месяца", value: "4month"},
+            {title: "5 месяцев", value: "5month"},
+            {title: "6 месяцев", value: "6month"},
+            {title: "7 месяцев", value: "7month"},
+            {title: "8 месяцев", value: "8month"},
+            {title: "9 месяцев", value: "9month"},
+            {title: "10 месяцев", value: "10month"},
+            {title: "11 месяцев", value: "11month"}
+        ])
+    }, "NotifyStandard");
+    content.addData({}, "GroupBottom");
 }
 
 function dead(state, rc, content){
     content.addData({
-        title: "Дата исполнения",
+        title: "Дата:",
         padding: "12,10,0,15",
         arg: "standard",
         standard: {
             "key": "notify",
-            "value": ["standard", "once", "cycle"],
+            "value": ["standard", "once", "cycle", "custom"],
             "trueCondition": "true",
             "falseCondition": "false"
         }
@@ -308,7 +456,7 @@ function dead(state, rc, content){
 
     content.addData({
         type: "datetime",
-        hint: "Дата исполнения",
+        hint: "",
         label: "",
         data: "",
         name: "deadLineDate",
@@ -316,7 +464,7 @@ function dead(state, rc, content){
         arg: "standard",
         standard: {
             "key": "notify",
-            "value": ["standard", "once", "cycle"],
+            "value": ["standard", "once", "cycle", "custom"],
             "trueCondition": "true",
             "falseCondition": "false"
         }
@@ -324,12 +472,12 @@ function dead(state, rc, content){
     }, "TextEdit", "NotifyStandard");
 
     content.addData({
-        title: "Время исполнения",
+        title: "Время:",
         padding: "12,15,0,15",
         arg: "standard",
         standard: {
             "key": "notify",
-            "value": ["standard", "once", "cycle"],
+            "value": ["standard", "once", "cycle", "custom"],
             "trueCondition": "true",
             "falseCondition": "false"
         }
@@ -337,7 +485,7 @@ function dead(state, rc, content){
 
     content.addData({
         type: "time",
-        hint: "Время исполнения",
+        hint: "",
         label: "",
         data: "",
         name: "deadLineTime",
@@ -345,10 +493,77 @@ function dead(state, rc, content){
         arg: "standard",
         standard: {
             "key": "notify",
-            "value": ["standard", "once", "cycle"],
+            "value": ["standard", "once", "cycle", "custom"],
             "trueCondition": "true",
             "falseCondition": "false"
         }
     }, "TextEdit", "NotifyStandard");
 
+}
+
+function custom(state, rc, content){
+    content.addData({
+        height: 30,
+        width: 10,
+        arg: "standard",
+        standard: {
+            "key": "notify",
+            "value": ["custom"],
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "SizedBox", "NotifyStandard");
+
+    content.addData({
+        title: "Добавить",
+        icon: "add",
+        onPressed: content.getMethod("joinAppStoreData", {joinAppStoreDataData: true}),
+        joinAppStoreDataData: {key: "custom_date", "append": "${deadLineDate} ${deadLineTime}\n"},
+        arg: "standard",
+        standard: {
+            "key": "notify",
+            "value": "custom",
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "ButtonBlue600", "NotifyStandard");
+
+    content.addData({
+        height: 30,
+        width: 10,
+        arg: "standard",
+        standard: {
+            "key": "notify",
+            "value": ["custom"],
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "SizedBox", "NotifyStandard");
+
+    content.addData({
+        type: "multiline",
+        hint: "",
+        label: "",
+        data: "",
+        name: "custom_date",
+        arg: "standard",
+        standard: {
+            "key": "notify",
+            "value": "custom",
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "TextEditAutofocusAppStore", "NotifyStandard");
+
+    content.addData({
+        title: "Выбирай произвольные даты и время - оповестим!!!",
+        padding: "10,17,10,10",
+        arg: "standard",
+        standard: {
+            "key": "notify",
+            "value": "custom",
+            "trueCondition": "true",
+            "falseCondition": "false"
+        }
+    }, "TextPadding", "NotifyStandard");
 }
