d85c9e9af335061c81212d4fbf13cd4c386a0a96
urasfinks <urasfinks@yandex.ru>
2022-09-14 18:31:58 +0300

    Update android add

--- src/main/webapp/request/sync-list/update.js
+++ src/main/webapp/request/sync-list/update.js
@@ -5,13 +5,23 @@ function main(state, rc, content) {
     content.setParentUI("WrapPage15");
     content.setWidgetData("backgroundColor", "#f5f5f5");
     content.addData({title: "Время пришло"}, "FirstH1-P-0-20");
-    if(rc.getPlatform() == "ios"){
+    if (rc.getPlatform() == "ios") {
         content.addData({
             title: "Обновить в AppStore",
             icon: "get_app",
             onPressed: ":launcher(onPressedLauncher)",
             onPressedLauncher: {
                 "url": "https://apps.apple.com/ru/app/mytodo/id1639810855"
+            }
+        }, "ButtonBlue600");
+    }
+    if (rc.getPlatform() == "android") {
+        content.addData({
+            title: "Скачать приложение",
+            icon: "get_app",
+            onPressed: ":launcher(onPressedLauncher)",
+            onPressedLauncher: {
+                "url": "https://jamsys.ru/apk/myTODO.apk"
             }
         }, "ButtonBlue600");
     }
